a0b693230e4d3a2d8069aa14c7ae5316193e47b8
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-10-17 22:11:07 -0300

    Update conversao_numerica.py

--- conversao_numerica.py
+++ conversao_numerica.py
@@ -1,15 +1,18 @@
 conversao = int(input('Digite o tipo de conversão quer fazer: \n1. Para decimal \n2. Para binário \n3. Para Hexadecimal \n'))
-tipo = int(input('Digite em qual base o número está: \n1. Binário \n2. Octal \n3. Hexadecimal\n'))
-numero = int(input('Digite o numero: '))
+tipo = int(input('Digite em qual base o número está: \n1. Decimal \n2. Binário \n3. Octal \n4. Hexadecimal\n'))
+numero = input('Digite o número: ')
 numero_convertido = ''
 base = 0
+
 if tipo == 1:
-   base = 2
+   base = 10
 elif tipo == 2:
-   base = 8
+   base = 2
 elif tipo == 3:
+   base = 8
+elif tipo == 4:
    base = 16
-decimal = 0
+
 decimal_para_hex = {
     0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5",
     6: "6", 7: "7", 8: "8", 9: "9", 10: "A", 11: "B",
@@ -21,26 +24,32 @@ hex_para_decimal = {
     "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15
 }
 
-if base == 16:
-    numero_invertido = str(numero).upper()[::-1]
-else:
-    numero_invertido = [int(x) for x in str(numero)[::-1]]
+numero_invertido = str(numero).upper()[::-1]
+numero_convertido = 0
 
-if conversao == 1:
-   for i in range(len(numero_invertido)):
-      decimal += numero_invertido[i] * (tipo ** i)
-   print(decimal)
-elif conversao == 2:
-   while True:
+if conversao == 1: #Para Decimal
+   if base == 16:
+      for i in range(len(numero_invertido)):
+         numero_convertido += hex_para_decimal[numero_invertido[i]] * (base ** i)
+   else:
+      for i in range(len(numero_invertido)):
+         numero_convertido += int(numero_invertido[i]) * (base ** i)
+   print('Em decimal:', numero_convertido)
+
+elif conversao == 2: #Para binário
+   numero = int(numero)
+   while numero > 0:
       numero_convertido = str(numero % 2) + numero_convertido
       numero = numero // 2
-      if numero == 0:
-         break
-elif conversao != 1 and conversao != 2:
-    print('Digite um número válido!!')
-print(numero_convertido)
-      
+   print('Em binário:', numero_convertido)
 
-
+elif conversao == 3:  # Para hexadecimal
+   numero = numero_convertido
+   numero_convertido = ''
+   while numero > 0:
+      resto = numero % 16
+      numero_convertido = decimal_para_hex[resto] + numero_convertido
+      numero //= 16
+   print('Em hexadecimal:', numero_convertido)
 
 
